80b4844a376350e6fbfaede11fc0ad02cb3983e3
Emily Joy DeHaan <emilydehaan001@gmail.com>
2022-03-26 07:06:21 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2;
 
-// Write your code here
+let random =Math.floor(Math.random() *10) +1;
+
+let mod;
+let num3 = 24;
+let num4 = 5;
+mod = num3 % num4;
+
+
+let max = (Math.max(1, 5, 6, 10, 20));
